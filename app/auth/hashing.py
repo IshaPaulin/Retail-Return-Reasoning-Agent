646c2f7259d2f8cb6f3a5bedb02915b8e69eefc1
nipun66 <82897957+nipun66@users.noreply.github.com>
2026-06-17 11:45:31 +0530

    Update hashing.py

--- app/auth/hashing.py
+++ app/auth/hashing.py
@@ -5,8 +5,3 @@ def hash_password(plain_password:str) ->str:
 
 def verify_password(plain_password:str,hashed_password:str)->bool:
     return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
-
-
-
-#hello world
-print("Hello World")
