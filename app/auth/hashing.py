0facf21d73e8f66059454c194784f3d6bbe760a0
Niranjana-A2006 <niranjanaa.ixb@gmail.com>
2026-06-17 11:28:34 +0530

    fixing errors -1

--- app/auth/hashing.py
+++ app/auth/hashing.py
@@ -1,9 +1,7 @@
-from passlib.context import CryptContext
-
-pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 def hash_password(plain_password:str) ->str:
-    return pwd_context.hash(plain_password)
+    return bcrypt.hashpw(plain_password.encode("utf-8"), bcrypt.gensalt(rounds=12)).decode("utf-8")
 
 def verify_password(plain_password:str,hashed_password:str)->bool:
-    return pwd_context.verify(plain_password,hashed_password)
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
